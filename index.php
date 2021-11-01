bba12bf589801d0cfd45a831f94706c475f5e98d
IsmaelHernandez <ismael.hernandez@upam.edu.mx>
2021-11-01 15:47:11 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!-- <!DOCTYPE html>
+<html>
+    <head>
+        <title>Prueba ismael</title>
+    </head>
+    <body>
+        <h1><?php echo "hola isma"?></h1>
+    </body>
+</html> -->
+
+<!-- variables -->
+<?php
+    $dato1 = 23;
+    echo $dato1;
+?>
